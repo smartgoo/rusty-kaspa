edb621b01a4216e1cc8a4bc772f8400eaa44e15d
cdvallone <cvallone28@gmail.com>
2024-04-13 07:26:52 -0400

    public/private keypair check in

--- consensus/python/src/keypair.rs
+++ consensus/python/src/keypair.rs
@@ -1,5 +1,9 @@
-use secp256k1::{Secp256k1};
+use secp256k1::{Secp256k1, XOnlyPublicKey};
+use std::error::Error;
+use std::fmt;
+use std::str::FromStr;
 
+use pyo3::exceptions::PyException;
 use pyo3::prelude::*;
 
 #[pyclass]
@@ -19,9 +23,52 @@ impl From<&secp256k1::SecretKey> for PrivateKey {
     }
 }
 
+#[pymethods]
 impl PrivateKey {
     #[new]
-    pub fn try_new(key: &str) -> PyResult<PrivateKey> {
-        Ok(Self { inner: secp256k1::SecretKey::from_str(key)? })
+    pub fn try_new(key: &str) -> PyResult<Self> {
+        let secret_key = secp256k1::SecretKey::from_str(key).map_err(|e| {
+            PyErr::new::<PyException, _>(format!("Failed to parse secret key: {}", e))
+        })?;
+        
+        Ok(Self { inner: secret_key })
+    }
+
+    #[pyo3(name = "to_string")]
+    pub fn to_hex(&self) -> String {
+        use kaspa_utils::hex::ToHex;
+        self.secret_bytes().to_vec().to_hex()
+    }
+
+    // TODO pub fn to_keypair
+}
+
+#[pyclass]
+pub struct PublicKey {
+    xonly_public_key: XOnlyPublicKey,
+    source: String,
+}
+
+// START: TESTING
+impl From<secp256k1::Error> for pyo3::PyErr {
+
+}
+
+// END TESTING
+
+#[pymethods]
+impl PublicKey {
+    #[new]
+    pub fn try_ney(key: &str) -> PyResult<Self> {
+        match secp256k1::PublicKey::from_str(key) {
+            Ok(public_key) => {
+                let (xonly_public_key, _) = public_key.x_only_public_key();
+                Ok(Self { xonly_public_key, source: (*key).to_string() })
+            }
+            Err(_e) => Ok(Self { 
+                xonly_public_key: XOnlyPublicKey::from_str(key)?, 
+                source: (*key).to_string() 
+            }),
+        }
     }
 }
